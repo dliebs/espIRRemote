6376bf5e0df9140e0a88ca8943ba956d67bee80b
dliebs <devin.lieberman@gmail.com>
2024-07-15 15:29:59 -0500

    Version 1.0.8 - Added Sony Shutter Example

---
+++ examples/sonyShutter/sonyShutter.ino
@@ -0,0 +1,38 @@
+//
+//
+//  ESP8266 Sony IR Shutter Button
+//
+//  Modified from espir.6.c
+//
+//  This version has not been deployed
+//
+//
+
+#include <IRsend.h>
+
+
+/*--------          GPIO          ----------*/
+/*--   Button attached to RESET and GND   --*/
+
+const uint16_t IRLED = 2;  // GPIO2, D1D4 (change this)
+IRsend irsend(IRLED);
+
+
+/*---------------- IR Codes ----------------*/
+// Both codes are 20-bit
+
+static const uint64_t shutter = 0xB4B8F;
+static const uint64_t shutterTwoSec = 0xECB8F;
+
+
+/*--------   Main Functions    ----------*/
+
+void setup() {
+  irsend.begin();
+  irsend.sendSony(shutter, 20, 2);  // 20-bit, 2x
+  ESP.deepSleep(0);
+}
+
+void loop() {
+  // Nothing Happens Here
+}
